e69d727088e51348f3567380ad5ddf3310d0d6f5
Vipul Kumar <codevipul42@gmail.com>
2026-02-22 16:38:56 +0530

    secured mongo uri using env

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,13 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://codevipul42:vipulMongo%40123@cluster0.x2bdk.mongodb.net/Food-Del', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URI);
         console.log("DB Connected");
     } catch (error) {
         console.error("DB Connection Error:", error);
-        process.exit(1); // Exit process with failure
+        process.exit(1);
     }
 };
